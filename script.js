f442bbf0a107f0c86c8d071ac23e2f67c93cd5b6
Charles Chavis <chavisca@gmail.com>
2023-07-09 16:46:57 -0400

    removed extraneous comment text from HTML and JS files, added screenshot

--- script.js
+++ script.js
@@ -2,30 +2,9 @@ var currentDayEl = $('#currentDay');
 var idList = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
 var buttons = document.querySelectorAll('.btn saveBtn');
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function() {
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?  Solved in analyzeTimeandModifyClasses function.
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-// Sets current time in DOM then month and ordinal day format
+// Sets current time in DOM then month and formatS
 function displayTime() {  
   var rightNow = dayjs().format('dddd, MMMM DD [at] hh:mm A');
   currentDayEl.text(rightNow);
